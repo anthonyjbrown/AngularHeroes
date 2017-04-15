3159e3b03519715e9cfd19d087639227e13949f1
anthonyjbrown <anthonybrown@Anthonys-iMac.home>
2017-04-15 13:59:10 +0100

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,49 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
-  imports: [ 
+  imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    HeroDetailComponent 
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
